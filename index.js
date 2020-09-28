5d89c5410bfd093cf9616530a5bb4012467b4c31
zDrar <44430949+bydrar@users.noreply.github.com>
2020-09-28 17:08:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require ('discord.js');
+const config = require("./config.json"); //make a 'config.json'// crea un archivo 'config.sjon' //
+const client = new Discord.Client();
+ 
+client.on('ready', ()=> {
+  console.log('bot is ready!');
+  client.user.setActivity('Tuto by zTa', {type: 'STREAMING', url:"https://youtube.com/zDrarMc"}).catch
+});
+ 
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(!message.content.startsWith(config.prefix)) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(command === 'ping'){
+  let ping = Math.floor(message.client.ws.ping); 
+  message.channel.send('➡`' +ping +' ms.` desde su servidor.')
+  }
+  
+  if(command === 'say'){ 
+  const SayCMD = args.join(" "); // lo que introduzcas despues del comando se enviará
+    message.delete().catch(O_o=>{}); //bot delete your text// el bot elimina tu mensaje
+    message.channel.send(SayCMD); //bot send your text// el bot manda tu mensaje
+  }
+  
+});
+ 
+client.login(config.token);
